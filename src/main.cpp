5b1fe8ff735e30ec266da0c4f08308b545c5d696
Jukka Vatjus-Anttila <jukka.vatjus-anttila@cie.fi>
2012-02-03 22:21:41 +0200

    Gameloop now actually works.

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,15 @@
  *
  * Created on January 23, 2012, 8:02 PM
  */
-//#include <iostream>
+#include <iostream>
 #include <cstdlib>
-
 #include "gameEntity.h"
 #include "towerEntity.h"
 #include "enemyEntity.h"
 #include "cMapper.h"
-#include <ctime>
+#include <SFML/System.hpp>
 
 using namespace std;
-
 /*
  * 
  */
@@ -22,52 +20,54 @@ int main(int argc, char** argv) {
 
     /// Just testing classes
     cout << "Hello tower defense!" << endl;
+    
+    // Initialize variables for gameloop timer logic
+    sf::Clock clock;
+    const int framerate = 1;
+    float framestartTime = 0;
+    float difference = 0;
+    float sleepTime = 0;
+    float frametime = 1/framerate;
+    
+    // Singleton mapper class which updates all gameEntities
+    cMapper *mapper;
 
-   clock_t start;           // timer test
-   start = clock();         // timer test
-   double diff = 0;             // timer test
-   static int frames = 0;   // timer test
-   int delta = 0;           // timer test
-   int framerate = 1;
+    try {
+        mapper = cMapper::getInstance();
+    } catch (bad_alloc&) {
+        return EXIT_FAILURE;
+    }
 
+    // Add couple gameEntities to mapper
+    mapper->add(dynamic_cast<gameEntity*> (new towerEntity(mapper)));
+    mapper->add(dynamic_cast<gameEntity*> (new enemyEntity(mapper)));
 
-   //   for ( int i = 0; i < 100000; i++ )   // timer test
-   //       cout << i << "\n";               // timer test
+    // Gameloop
+    while(1)
+    {
+        // Get current framestart time
+        framestartTime = clock.GetElapsedTime();
+        // Update game logic. Here only mapper->update()
+        mapper->update(framestartTime);
+        // Get time elapsed in game logic update
+        difference = clock.GetElapsedTime() - framestartTime;
+        
+        // If difference is smaller than budgeted, rest until next frame
+        if (difference < frametime)
+        {
+            // Rendering budgets:
+            // 60fps = 16,667ms
+            // 30fps = 33,333ms
+            // 10fps = 100ms
+            //  5fps = 200ms
+            sleepTime = frametime - difference;
+            // Sleep for rest of the frame
+            sf::Sleep(sleepTime);
+        }
+    }
+    // Target has been acquired on last update so now fire sequence initiates.
+    delete mapper;
 
-   cMapper *mapper;
-
-   try {
-       mapper = cMapper::getInstance();
-   }
-   catch (bad_alloc&)
-   {
-       return EXIT_FAILURE;
-   }
-
-   mapper->add(dynamic_cast<gameEntity*>(new towerEntity(mapper)));
-   mapper->add(dynamic_cast<gameEntity*>(new enemyEntity(mapper)));
-   // This update does not initiate fire sequence on tower entity.
-
-   while (1)
-   {
-       start = ( clock() - start );
-       cout << "Clock(): " << clock() << " Start: " << start << " ClocksPerSec: " << (double)CLOCKS_PER_SEC << "\n";
-
-       mapper->update(start);
-
-       // Testing timer
-       diff = ( start - diff );
-       cout << diff << "\n";
-       if ((1000/framerate - diff) > 0)
-       {
-           double sleeptime = (1000/framerate - diff);
-           cout << "Sleeping " << sleeptime << "\n";
-           sleep(sleeptime/1000);
-       }
-   }
-   // Target has been acquired on last update so now fire sequence initiates.
-   delete mapper;
-
-   return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
