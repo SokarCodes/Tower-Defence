842e9141622fe8c0c7ad77c203753ad808da6489
Jukka Vatjus-Anttila <Jukka.vatjusanttila@gmail.com>
2012-02-25 17:36:49 +0200

    Event handling and removed profiling symbols.

--- src/EventHandler/cEventHandler.cpp
+++ src/EventHandler/cEventHandler.cpp
@@ -43,7 +43,7 @@ void cEventHandler::update()
             appRunning = false;
             render_->getRenderwindow()->Close();
             break;
-        // Keyboard key pressed
+            // Keyboard key pressed
         case sf::Event::KeyPressed:
             switch (event_.Key.Code)
             {
@@ -80,20 +80,61 @@ void cEventHandler::update()
                 std::cout << "No action for key.\n";
             }
             break;
-        // Mouse button pressed
+            // Mouse button pressed
         case sf::Event::MouseButtonPressed:
             switch (event_.MouseButton.Button)
             {
             case sf::Mouse::Left:
+                std::cout << "Mouse left!\n";
                 break;
             case sf::Mouse::Right:
+                std::cout << "Mouse right!\n";
+                break;
+            case sf::Mouse::Middle:
+                std::cout << "Mouse middle!\n";
                 break;
             default:
                 std::cout << "No action for mousebutton.\n";
             }
             break;
-        default:
-            std::cout << "Input type not implemented!\n";
+        case sf::Event::Resized:
+            break;
+        case sf::Event::LostFocus:
+            break;
+        case sf::Event::GainedFocus:
+            break;
+        case sf::Event::TextEntered:
+            break;
+        case sf::Event::KeyReleased:
+            break;
+        case sf::Event::MouseWheelMoved:
+            switch (event_.MouseWheel.Delta)
+            {
+            case -1:
+                std::cout << "Mouse wheen down.\n";
+                break;
+            case 1:
+                std::cout << "Mouse wheen up.\n";
+                break;
+            }
+            break;
+        case sf::Event::MouseButtonReleased:
+            break;
+        case sf::Event::MouseMoved:
+            break;
+        case sf::Event::MouseEntered:
+            break;
+        case sf::Event::MouseLeft:
+            // This event is already handled in mousebuttonPressed
+            break;
+        case sf::Event::Count:
+            break;
+        case sf::Event::JoyButtonPressed:
+            break;
+        case sf::Event::JoyButtonReleased:
+            break;
+        case sf::Event::JoyMoved:
+            break;
         }
     }
 }
